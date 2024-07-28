66300fd12d3f54990def46b568746a4acc236bc8
WailTech <116460489+WailApp@users.noreply.github.com>
2024-07-28 12:47:32 +0100

    Add files via upload

--- auth-check.js
+++ auth-check.js
@@ -1,27 +1,31 @@
-// Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyAzg4PmFoXnS95TXk8FlG9C4bSxhfer86E",
-    authDomain: "wailai-a.firebaseapp.com",
-    projectId: "wailai-a",
-    storageBucket: "wailai-a.appspot.com",
-    messagingSenderId: "857204915249",
-    appId: "1:857204915249:web:0141c2e2e68ed96769e910",
-    measurementId: "G-VRR62LMMMK"
-};
+// Check if Firebase has been initialized already
+if (!firebase.apps.length) {
+    // Firebase configuration
+    const firebaseConfig = {
+        apiKey: "AIzaSyAzg4PmFoXnS95TXk8FlG9C4bSxhfer86E",
+        authDomain: "wailai-a.firebaseapp.com",
+        projectId: "wailai-a",
+        storageBucket: "wailai-a.appspot.com",
+        messagingSenderId: "857204915249",
+        appId: "1:857204915249:web:0141c2e2e68ed96769e910",
+        measurementId: "G-VRR62LMMMK"
+    };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+}
 
-// Get reference to auth service
-const auth = firebase.auth();
+// Check if auth is already defined
+if (typeof window.auth === 'undefined') {
+    window.auth = firebase.auth();
+}
 
-// Check user authentication status
-auth.onAuthStateChanged(user => {
-    if (!user) {
-        // User is not signed in, redirect to login page
-        window.location.href = 'login.html'; // URL of your login page
+// Example of usage in auth-check.js
+window.auth.onAuthStateChanged(user => {
+    if (user) {
+        console.log('User is signed in:', user.email);
     } else {
-        // User is signed in, proceed to the page
-        console.log("User is signed in:", user.email);
+        console.log('No user is signed in.');
+		window.location.href = 'login.html'; // URL of your login page
     }
 });
